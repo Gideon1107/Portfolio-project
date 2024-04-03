65ffc1d7812bbe759d3b917583496139d106344d
Gideon1107 <ayenigideon1107@gmail.com>
2024-04-03 01:28:32 +0100

    completed and fully responsive

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
+    sidebar.style.display = 'flex';
+
+    sidebar.style.transition = "opacity 0.3s ease, transform 0.3s ease";
+    
+    
+    setTimeout(function() {
+        sidebar.style.opacity = "1";
+        sidebar.style.transform = "translateX(0)";
+
+        setTimeout(function() {
+            sidebar.style.transition = "";
+        },400);
+    }, 100);
+    
   }
 function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
